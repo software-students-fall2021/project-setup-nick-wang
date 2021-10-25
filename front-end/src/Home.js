ed4db1912b8ef055d73b5f57ae54f8a081d1e672
Zack Ye <ly1339@nyu.edu>
2021-10-25 18:26:21 -0400

    Add DiaryOverview page

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -28,7 +28,7 @@ const Home = (props) => {
           <Divider vertical>Or</Divider>
           <Grid.Row verticalAlign="middle">
             <Grid.Column>
-              <Header icon as={Link} to="/">
+              <Header icon as={Link} to="/diary_overview">
                 <Icon name="pencil alternate" />
                 Diary
               </Header>
