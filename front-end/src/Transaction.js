f42589e86837921b0dd7d1642f169e0d34212585
yym1928 <yimingyu919@gmail.com>
2021-10-26 20:11:07 -0400

    add dollar sign on display

--- front-end/src/Transaction.js
+++ front-end/src/Transaction.js
@@ -5,7 +5,7 @@ function Transaction(props){
         <article className="transaction">
             <div className="name">{props.details.name}</div>
             <div className="date">{props.details.date}</div>
-            <div className="amount">{props.details.amount}</div>
+            <div className="amount">{"$"+props.details.amount}</div>
             <div className="trscType">{props.details.type}</div>
         </article>
     )
