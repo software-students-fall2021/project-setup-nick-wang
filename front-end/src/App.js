ce64e1074a3339d9acd3f5d612e121021b5c7aee
Susan0223 <sl6994@nyu.edu>
2021-10-28 22:53:46 -0400

    Diary Detail Page Added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,19 +3,18 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import PrimaryNav from './PrimaryNav'
 // import Home from './Home'
 import Diary from './Diary/Diary'
+import DiaryDetail from './Diary/components/DiaryDetail/DiaryDetail'
 import "./App.css";
 
 function App() {
+
   return (
-    <div className="container">
       <Router>
         <Switch>
-          <Route path="/">
-            <Diary />
-          </Route>
+          <Route path="/" exact component={Diary} />
+          <Route path="/Detail" component={DiaryDetail} />
         </Switch>
       </Router>
-    </div>
   );
 }
 
---
+++ front-end/src/Diary/components/DiaryStack/DiaryStack.js
@@ -0,0 +1,92 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import { CardActionArea, Grid } from '@mui/material';
+import { Typography } from '@mui/material';
+
+export default function DiaryStack(props) {
+    
+    let diaries = []
+    const [isEditing, setIsEditing] = React.useState(false);
+    const numOfDates = monthToNumOfDates(props.pickedDate);
+    const year = props.pickedDate.getYear();
+    const month = props.pickedDate.getMonth() + 1;
+
+    let diariesContent = []
+    for(var i = 0; i < numOfDates; i++){
+        diariesContent.push('');
+    }
+
+    const [value, setValue] = React.useState(diariesContent);
+
+    const toggleIsEditing = () => setIsEditing((b) => !b);
+    const setIthValue = (idx) => {
+        return (e) => {
+            const newValue = [];
+            for (var i =0;i<numOfDates; ++i) {
+                if (i !== idx) {
+                    newValue.push(value[i]);
+                }else {
+                    newValue.push(e.value);
+                }
+            }
+            setValue(newValue);
+        };
+    };
+    for (var i = 0; i < numOfDates; i++) {
+        const onChange = setIthValue(i);
+        diaries.push(
+        <Card
+         fullWidth={true}
+         sx={{
+             backgroundColor:'#FFFAF0',
+             width:'250px',
+             height: '250px',
+             boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
+             transition: '0.3s',
+             marginRight: '20px',
+             marginLeft: '20px',
+             marginTop: '20px',
+         }}
+        >
+            <CardActionArea href="/Detail">
+                <Typography
+                 sx={{
+                  fontSize:'larger',
+                  textAlign: 'center',
+                  paddingTop:'10px',
+                  width:'250px',
+                  height: '250px'
+                 }}>
+                    {month}/{i + 1}
+                </Typography>
+                <Typography style={{ display: "none" }} >
+                </Typography>
+            </CardActionArea>
+        </Card>);
+    }
+    return (
+      <div>
+        <Grid 
+        container justifyContent="center"
+        >
+            {diaries}
+        </Grid>
+      </div>
+    );
+}
+
+const monthToNumOfDates = (date) => { 
+    const year = date.getYear()
+    const month = date.getMonth()
+    const numOfDates = [31, 28, 31, 30,31,30,31,31,30,31,30,31];
+    let result = 0;
+    if(year % 4 != 0 ){
+        result = numOfDates[month]
+    }
+    else{
+        if(month != 1) result = numOfDates[month]
+        else
+            result = 29
+    }
+    return result
+}
