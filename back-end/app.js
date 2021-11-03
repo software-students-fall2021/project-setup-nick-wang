5ac782a378f92a6223a08ffa299b28e56c7d480c
Letian Ye <ly1339@nyu.edu>
2021-11-02 23:01:56 -0400

    Add basic framework of back-end, add an example of diary overview router

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,36 @@
-// import and instantiate express
 const express = require("express") // CommonJS import style!
 const app = express() // instantiate an Express object
-// we will put some server logic here later...
+const path = require("path")
+
+// import some useful middleware
+const multer = require("multer") // middleware to handle HTTP POST requests with file uploads
+const axios = require("axios") // middleware for making requests to APIs
+// require("dotenv").config({ silent: true }) // load environmental variables from a hidden file named .env
+const morgan = require("morgan")
+// require cors
+const cors = require("cors")
+
+const diaryWordCloud = require("./routes/Diary/Overview")
+
+
+// use the morgan middleware to log all incoming http requests
+app.use(morgan("dev")) // morgan has a few logging default styles - dev is a nice concise color-coded style
+// use express's builtin body-parser middleware to parse any data included in a request
+app.use(express.json()) // decode JSON-formatted incoming POST data
+app.use(express.urlencoded({ extended: true })) // decode url-encoded incoming POST data
+// make 'public' directory publicly readable with static content
+app.use("/static", express.static("public"))
+// use cors
+app.use(cors())
+
+// ==============================================================
+// route for HTTP GET requests to the root document
+app.use("/", diaryWordCloud)
+
+app.get("/", (req, res) => {
+  res.send("Hello world!")
+})
+
+// ==============================================================
 // export the express app we created to make it available to other modules
-module.exports = app
+module.exports = app // CommonJS export style!
--- back-end/server.js
+++ back-end/server.js
@@ -1,14 +1,22 @@
 #!/usr/bin/env node
-const server = require("./app") // load up the web server
-const port = 3000 // the port to listen to for incoming requests
-// call express's listen function to start listening to the port
+
+// import the express app
+const server = require('./app')
+
+// which port to listen for HTTP(S) requests
+const port = 9000
+
+// call a function to start listening to the port
 const listener = server.listen(port, function () {
   console.log(`Server running on port: ${port}`)
 })
+
 // a function to stop listening to the port
 const close = () => {
   listener.close()
 }
+
+// export the close function
 module.exports = {
   close: close,
 }
