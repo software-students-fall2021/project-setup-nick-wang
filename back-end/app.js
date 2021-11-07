2f2521ce9642f21f3c97fe9f281bc94f86545e1c
bloombar <amos@cs.nyu.edu>
2021-11-07 00:03:37 -0400

    not using formData anymore

--- back-end/app.js
+++ back-end/app.js
@@ -1,77 +1,86 @@
 // import and instantiate express
 const express = require("express") // CommonJS import style!
-const cors = require('cors')
-const bodyParser = require("body-parser")
+const cors = require("cors")
+const morgan = require("morgan")
 //const axios = require("axios")
+
 const app = express() // instantiate an Express object
+
 // we will put some server logic here later...
 // export the express app we created to make it available to other modules
 app.use(express.json()) // decode JSON-formatted incoming POST data
 app.use(express.urlencoded({ extended: true })) // decode url-encoded incoming POST data
 app.use(cors())
+app.use(morgan("dev")) // outputs each request for debugging
+
+// mark the public directory as all static files that do not require pre-processing
 app.use("/static", express.static("public"))
-// listens for any HTTP GET request for the / path, 
+
+// listens for any HTTP GET request for the / path,
 // and responds with the plain text, 'Hello!'
 app.get("/", (req, res) => {
-    res.send("Hello!")
-  })
+  res.send("Hello!")
+})
 
-  // send static file
-  const mockFile = [
-    {"name":"Steak", "date": "11/4/2021", "amount": 13, "type": "Food"},
-    {"name":"iphone", "date": "11/4/2021", "amount": 1000, "type": "School"},
-    {"name":"macbook pro", "date": "11/4/2021", "amount": 2000, "type": "School"},
-    {"name":"pencil", "date": "11/4/2021", "amount": 1.9, "type": "School"},
-    {"name":"shirt", "date": "11/4/2021", "amount": 50, "type": "Clothing"}
+// send static file
+const mockFile = [
+  { name: "Steak", date: "11/4/2021", amount: 13, type: "Food" },
+  { name: "iphone", date: "11/4/2021", amount: 1000, type: "School" },
+  { name: "macbook pro", date: "11/4/2021", amount: 2000, type: "School" },
+  { name: "pencil", date: "11/4/2021", amount: 1.9, type: "School" },
+  { name: "shirt", date: "11/4/2021", amount: 50, type: "Clothing" },
 ]
-app.get("/static-file", (req, res) =>{
+
+app.get("/static-file", (req, res) => {
   // axios
   //   .get("transcation.json")
   //   .then(apiResponse => res.json(apiResponse.data)) // pass data along directly to client
   //   .catch(err => next(err)) // pass any errors to express
   res.json(mockFile)
 })
+
 // search transaction function; post from client
 app.post("/post-search", (req, res) => {
   console.log(JSON.stringify(req.body, null, 2))
-    // now do something amazing with the data we received from the client
-    const data = {
-      status: "amazing success!",
-      message: "congratulations on searching this transaction: " + req.body.search,
-      your_data: {
-        search: req.body.search,
-      },
-    }
-    res.json(data);
-    //res.redirect("/")
-  })
+  // now do something amazing with the data we received from the client
+  const data = {
+    status: "amazing success!",
+    message:
+      "congratulations on searching this transaction: " + req.body.search,
+    your_data: {
+      search: req.body.search,
+    },
+  }
+  res.json(data)
+  //res.redirect("/")
+})
 
 // add transaction function; post from client
 app.post("/post-add", (req, res) => {
-    console.log(req.body.trscName)
-    console.log(JSON.stringify(req.body, null, 2))
-    // now do something amazing with the data we received from the client
-    const data = {
-      status: "amazing success!",
-      message: "congratulations on adding the transaction!",
+  // console.log(req.body.trscName)
+  console.log(`post data: ${JSON.stringify(req.body, null, 2)}`)
+  // now do something amazing with the data we received from the client
+  const data = {
+    status: "amazing success!",
+    message: "congratulations on adding the transaction!",
 
-      your_data: {
-        trscName: req.body.trscName,
-        trscAmount: req.body.trscAmount,
-        trscType: req.body.trscType,
-      },
-    }
-    // console.log(JSON.stringify(data, null, 2))
-    res.json(data);
-    //res.redirect("/")
-  })
+    your_data: {
+      trscName: req.body.trscName,
+      trscAmount: req.body.trscAmount,
+      trscType: req.body.trscType,
+    },
+  }
+  // console.log(JSON.stringify(data, null, 2))
+  res.json(data)
+  //res.redirect("/")
+})
 
 // proxy requests to/from an API
 app.get("/proxy-example", (req, res, next) => {
-    // use axios to make a request to an API for animal data
-    axios
-      .get("http://localhost:9000/post-search")
-      .then(apiResponse => res.json(apiResponse.data)) // pass data along directly to client
-      .catch(err => next(err)) // pass any errors to express
-  })
+  // use axios to make a request to an API for animal data
+  axios
+    .get("http://localhost:9000/post-search")
+    .then(apiResponse => res.json(apiResponse.data)) // pass data along directly to client
+    .catch(err => next(err)) // pass any errors to express
+})
 module.exports = app
--- front-end/src/AccountBook/AccountBookMain.js
+++ front-end/src/AccountBook/AccountBookMain.js
@@ -1,170 +1,200 @@
-import React, { useEffect, useState } from "react";
-import { PieChart } from 'react-minimal-pie-chart';
-import { Link } from "react-router-dom";
-import Transaction from "./Transaction";
-import "./AccountBookMain.css";
-import axios from "axios";
-import {useHistory} from "react-router-dom";
+import React, { useEffect, useState } from "react"
+import { PieChart } from "react-minimal-pie-chart"
+import { Link } from "react-router-dom"
+import Transaction from "./Transaction"
+import "./AccountBookMain.css"
+import axios from "axios"
+import { useHistory } from "react-router-dom"
 
+function AccountBookMain() {
+  const [data, setData] = useState([])
+  const history = useHistory()
 
-function AccountBookMain(){
-    const [data, setData] = useState([]);
-    const history = useHistory();
+  useEffect(() => {
+    async function fetchData() {
+      const result = await axios(
+        //"https://my.api.mockaroo.com/test2.json?key=9c8bd880"
+        // "https://my.api.mockaroo.com/users.json?key=157fcab0"
+        "http://localhost:9000/static-file"
+      )
 
-    useEffect(() => {
-        async function fetchData(){
-            const result = await axios(
-                //"https://my.api.mockaroo.com/test2.json?key=9c8bd880"
-                // "https://my.api.mockaroo.com/users.json?key=157fcab0"
-                "http://localhost:9000/static-file"
-            );
+      setData(result.data)
+    }
+    fetchData()
+  }, [])
 
-            setData(result.data)
-        }
-        fetchData();
-    }, [])
+  const [status, setStatus] = useState({})
 
-    const [status, setStatus] = useState({})
-    const handleSubmit = async e => {
-        // prevent html form from submiting and reloading
-        e.preventDefault();
+  const handleSubmit = async e => {
+    // prevent html form from submiting and reloading
+    e.preventDefault()
 
-        // get data
-        const name = e.target.trscName.value
-        const amount = e.target.trscAmount.value
-        const type = e.target.trscType.value
+    try {
+      // send a post request to the server
+      const requestData = {
+        trscName: e.target.trscName.value,
+        trscAmount: e.target.trscAmount.value,
+        trscType: e.target.trscType.value,
+      }
+      const response = await axios.post(
+        "http://localhost:9000/post-add",
+        requestData
+      )
 
-        // send the data to api
-        const formData = new FormData()
-        formData.append('trscName', name)
-        formData.append('trscAmount', amount)
-        formData.append('trscType', type)
-        
-        for (var pair of formData.entries())
-        {
-            console.log(pair[0] + pair[1])
-        }
-        try {
-            // send the request to the server
-            const response = await axios({
-                method: "post",
-                url: "http://localhost:9000/post-add",
-                data: formData,
-                headers: { "Content-Type": "application/json" },
-            })
-            // const blog = { trscName, trscAmount, trscName}
-            // fetch('http://localhost:9000/post-add', {
-            //     method: 'POST',
-            //     headers: { "Content-Type": "multipart/form-data" },
-            //     body: JSON.stringify(blog)
-            // })
-            console.log(response.data)
-            setStatus(response.data)
-        }
-        catch (err) {
-            // throw an error
-            throw new Error(err)
-          }
-        }
-    
+      // const blog = { trscName, trscAmount, trscName}
+      // fetch('http://localhost:9000/post-add', {
+      //     method: 'POST',
+      //     headers: { "Content-Type": "multipart/form-data" },
+      //     body: JSON.stringify(blog)
+      // })
 
-    return (
-        <body className="body">
-            <h1></h1><h1></h1>
-            <header className="header">
-                <h1>Account Book</h1>
-            </header>
-            
-            <form action="http://localhost:9000/post-search" method="post">
-                <div className="search" >
-                    <input className="searchBar" type="text" id="search" name="search" placeholder="Search an transaction"></input>   
-                </div>
-            </form>
+      console.log(response.data)
+      setStatus(response.data)
+    } catch (err) {
+      // throw an error
+      throw new Error(err)
+    }
+  }
 
-            <form onSubmit={handleSubmit}>
-                <div className="add">
-                    <h2>Add a transaction</h2>
-                    <input className="addBar" type="text" id="trscName" name="trscName" placeholder="Description"></input>
-                    <input className="addBar" type="number" id="trscAmount" name="trscAmount" step=".01" placeholder="$ Amount"></input>
-                    <input className="addBar" type="text" id="trscType" name="trscType" placeholder="Type"></input>
-                    <input type="submit" name="submit" value="Add"></input>
-                </div>
-            </form>
+  return (
+    <body className="body">
+      <h1></h1>
+      <h1></h1>
+      <header className="header">
+        <h1>Account Book</h1>
+      </header>
 
-            <div className="transactions">
-                <>
-                <h2>Recent Transactions</h2>
-                <section className="">
-                    {data.map(item => (
-                    <Transaction key={item.id} details={item} />
-                    ))}
-                </section>
-                </>
+      <form action="http://localhost:9000/post-search" method="post">
+        <div className="search">
+          <input
+            className="searchBar"
+            type="text"
+            id="search"
+            name="search"
+            placeholder="Search an transaction"
+          ></input>
+        </div>
+      </form>
 
-                <Link className="moreTranscations" to="/account_book/overview">More Transactions</Link>
-            </div>
-            
-            <div className="summary">
-                <h2 className="spendingSumHead">Your Spending Summary</h2>
-                <div className="pieChart">
+      <form onSubmit={handleSubmit}>
+        <div className="add">
+          <h2>Add a transaction</h2>
+          <input
+            className="addBar"
+            type="text"
+            id="trscName"
+            name="trscName"
+            placeholder="Description"
+          ></input>
+          <input
+            className="addBar"
+            type="number"
+            id="trscAmount"
+            name="trscAmount"
+            step=".01"
+            placeholder="$ Amount"
+          ></input>
+          <input
+            className="addBar"
+            type="text"
+            id="trscType"
+            name="trscType"
+            placeholder="Type"
+          ></input>
+          <input type="submit" name="submit" value="Add"></input>
+        </div>
+      </form>
 
-                    <PieChart radius="20"
-                        data={data.map(item => (
-                            {title: item.type, value: item.amount, color: item.color}
-                        ))}
-                            
-                        label={({ dataEntry }) => dataEntry.title}
-                        labelStyle={{
-                            fontSize: '6px',
-                            fontFamily: 'Impact',
-                            fill: 'grey'
-                        }}
-                        labelPosition={112}
-                    />
-    
-                    <PieChart radius="35"
-                        data={[{ value: 100, key: 1, color: 'green' }]}
-                        startAngle={270}
-                        reveal={50}
-                        lineWidth={30}
-                        background="grey"
-                        lengthAngle={360}
-                        rounded
-                        animate
-                        label={({ dataEntry }) => dataEntry.value}
-                        labelStyle={{
-                        fontSize: '10px',
-                        fontFamily: 'Impact',
-                        fill: 'grey',
-                        }}
-                        labelPosition={0}
-                    />
-                </div>
-            </div>
+      <div className="transactions">
+        <>
+          <h2>Recent Transactions</h2>
+          <section className="">
+            {data.map(item => (
+              <Transaction key={item.id} details={item} />
+            ))}
+          </section>
+        </>
 
-            <div className="allTypes">
-                <h2>Categories</h2>
-                <div className="typeRow">
-                    <Link className="type" to="/account_book/category">Type 1</Link>
-                    <Link className="type" to="/account_book/category">Type 2</Link>
-                </div>
+        <Link className="moreTranscations" to="/account_book/overview">
+          More Transactions
+        </Link>
+      </div>
 
-                <div className="typeRow">
-                    <Link className="type" to="/account_book/category">Type 3</Link>
-                    <Link className="type" to="/account_book/category">Type 4</Link>
-                </div>
+      <div className="summary">
+        <h2 className="spendingSumHead">Your Spending Summary</h2>
+        <div className="pieChart">
+          <PieChart
+            radius="20"
+            data={data.map(item => ({
+              title: item.type,
+              value: item.amount,
+              color: item.color,
+            }))}
+            label={({ dataEntry }) => dataEntry.title}
+            labelStyle={{
+              fontSize: "6px",
+              fontFamily: "Impact",
+              fill: "grey",
+            }}
+            labelPosition={112}
+          />
 
-                <div className="typeRow">
-                    <Link className="type" to="/account_book/category">Type 5</Link>
-                    <Link className="type" to="/account_book/category">Type 6</Link>
-                </div>
-            </div>
-            
-            <footer className="footerABM">
-                <p>&copy;2021 LifeNote Team</p>  
-            </footer>     
-        </body>
-    )
+          <PieChart
+            radius="35"
+            data={[{ value: 100, key: 1, color: "green" }]}
+            startAngle={270}
+            reveal={50}
+            lineWidth={30}
+            background="grey"
+            lengthAngle={360}
+            rounded
+            animate
+            label={({ dataEntry }) => dataEntry.value}
+            labelStyle={{
+              fontSize: "10px",
+              fontFamily: "Impact",
+              fill: "grey",
+            }}
+            labelPosition={0}
+          />
+        </div>
+      </div>
+
+      <div className="allTypes">
+        <h2>Categories</h2>
+        <div className="typeRow">
+          <Link className="type" to="/account_book/category">
+            Type 1
+          </Link>
+          <Link className="type" to="/account_book/category">
+            Type 2
+          </Link>
+        </div>
+
+        <div className="typeRow">
+          <Link className="type" to="/account_book/category">
+            Type 3
+          </Link>
+          <Link className="type" to="/account_book/category">
+            Type 4
+          </Link>
+        </div>
+
+        <div className="typeRow">
+          <Link className="type" to="/account_book/category">
+            Type 5
+          </Link>
+          <Link className="type" to="/account_book/category">
+            Type 6
+          </Link>
+        </div>
+      </div>
+
+      <footer className="footerABM">
+        <p>&copy;2021 LifeNote Team</p>
+      </footer>
+    </body>
+  )
 }
 
-export default AccountBookMain;
+export default AccountBookMain
