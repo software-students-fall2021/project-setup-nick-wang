d2c8c1f9b6e3e70b124a1cbff891c454d912716d
Letian Ye <ly1339@nyu.edu>
2021-11-15 21:31:06 -0500

    Add signin auth and sample secret auth

--- back-end/models/user.js
+++ back-end/models/user.js
@@ -1,4 +1,5 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
 // Create User schema
@@ -7,7 +8,7 @@ const userSchema = new Schema({
     type: String,
     required: true,
     unique: true,
-    lowercase: true
+    lowercase: true,
   },
   password: {
     type: String,
@@ -15,8 +16,29 @@ const userSchema = new Schema({
   },
 });
 
+userSchema.pre('save', async function (next) {
+  try {
+    // Generate a salt
+    const salt = await bcrypt.genSalt();
+    // hash: salt + hash
+    const passwordHash = await bcrypt.hash(this.password, salt);
+    this.password = passwordHash;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.isValidPassword = async function (newPassword) {
+  try {
+    return bcrypt.compare(newPassword, this.password);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
 // Create models
-const User = mongoose.model("user", userSchema);
+const User = mongoose.model('user', userSchema);
 
 // Export the model
 module.exports = User;
