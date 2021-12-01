001ee45fd605d582b5f3d58e3f270c9fdcdc8e33
Zhuofeng Wang <zw2922@nyu.edu>
2021-12-01 00:30:46 -0500

    Test written for transaction category page

--- back-end/test/AccountBookTransactionPageTest.spec.js
+++ back-end/test/AccountBookTransactionPageTest.spec.js
@@ -23,3 +23,23 @@ describe("Account Book Transaction Page Test", () => {
       });
     });
   });
+
+  describe("Account Book Transaction Categoty Page Test", () => {
+    describe("GET get transactions data", () => {
+      it("It should GET transaction data for the transaction pages", (done) => {
+        const type = "meat"
+        chai
+          .request(server)
+          .get("/Transaction_data/" + type)
+          .end((err, response) => {
+            if (err) throw err;
+            response.should.have.status(200);
+            response.body[0].should.have.property("name");
+            response.body[0].should.have.property("date");
+            response.body[0].should.have.property("amount");
+            response.body[0].should.have.property("type");
+            done();
+          });
+      });
+    });
+  });
