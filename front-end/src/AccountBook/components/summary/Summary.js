fab7ba291087d3135927b1ee73c2df4032aa4f91
YaolinZhang <yz6813@nyu.edu>
2021-12-06 11:32:21 -0500

    fix bug for summary

--- front-end/src/AccountBook/components/summary/Summary.js
+++ front-end/src/AccountBook/components/summary/Summary.js
@@ -22,7 +22,7 @@ function Summary(props){
 
     useEffect(() => {
         axios
-        .get(`${process.env.REACT_APP_BACKEND}/users/secret`, {
+        .get(`/users/secret`, {
             headers: { authorization: jwtToken }, // pass the token, if any, to the server
         })
         .then((res) => {
