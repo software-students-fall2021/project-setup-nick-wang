9f50986501369ca1eafd4ba5bff22404684f8473
Susan0223 <sl6994@nyu.edu>
2021-12-05 19:29:29 -0500

    diary test fixed

--- back-end/test/DetailService.spec.js
+++ back-end/test/DetailService.spec.js
@@ -7,35 +7,39 @@ chai.use(chaiHTTP);
 
 describe("API Test", () => {
   describe("GET /Details/:date", () => {
-    it("It should GET the diary content by date", (done) => {
-      const date = "11-1-2021";
+    it("It should GET the diary content by date and username", (done) => {
+      const date = "admin/12-1-2021";
       chai
         .request(server)
         .get("/Details/" + date)
         .end((err, response) => {
           if (err) throw err;
           response.should.have.status(200);
+          response.body[0].should.have.property("username");
           response.body[0].should.have.property("date");
           response.body[0].should.have.property("content");
-          response.body[0].should.have.property("date").eq("11-1-2021");
+          response.body[0].should.have.property("username").eq("admin");
+          response.body[0].should.have.property("date").eq("12-1-2021");
           response.body[0].should.have
             .property("content")
-            .eq("today is 11-1-2021");
+            .eq("12345");
           done();
         });
     });
   });
 
   describe("PUT /Details/:date", () => {
-    it("It should update the diary content with given date and content", (done) => {
+    it("It should update the diary content with given username, date and content", (done) => {
+      const username = "admin";
       const date = "11-1-2021";
       const newDiary = {
+        username: username,
         date: date,
         content: "today is 11-1-2021",
     }
       chai
         .request(server)
-        .put("/Details/" + date)
+        .put("/Details/" + username + "/" + date)
         .send(newDiary)
         .end((err, response) => {
           if (err) throw err;
